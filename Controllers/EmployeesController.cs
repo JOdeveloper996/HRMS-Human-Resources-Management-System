19395c94fdaa7e9846255766e5c70cc4fc067539
mahmoud alwahsh <jodeveloper@yahoo.com>
2026-05-08 22:11:00 +0300

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS_Human_Resources_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
